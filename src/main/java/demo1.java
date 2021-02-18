38fe889c01481fa00faea9e1328743a740dadebd
swath-03 <64493233+swath-03@users.noreply.github.com>
2021-02-18 20:21:46 +0000

    added more methods to demo1.java

--- src/main/java/demo1.java
+++ src/main/java/demo1.java
@@ -11,10 +11,34 @@ public class demo1 {
         y = 200;
         c = x + y;
 
+
         System.out.println("welcome to Maven");
         System.out.println("this is a demo test");
 
         System.out.println("the sum of x + y = "+c);
     }
 
+    public int add(int a, int b,int c){
+
+
+        c = a + b;
+
+        System.out.println("the value in C is :"+c);
+        return c;
+    }
+
+    public int sub(int a,int b,int c){
+
+        c = a - b;
+
+        System.out.println("the value in C is :"+c);
+        return c;
+    }
+
+    public int mul(int a,int b,int c){
+
+        c = a *b;
+        System.out.println("the value in C is :"+c);
+        return c;
+    }
 }
--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -7,7 +7,13 @@ public class Test1 {
     public void demo1test()
     {
 
-    demo1 obj = new demo1();
+    demo1 obj1 = new demo1();
+
+    obj1.add(20,30,0);
+
+    obj1.sub(300,100,0);
+
+    obj1.mul(2,5,0);
 
     }
 }
