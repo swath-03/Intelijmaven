fbb022722b269fd7da4877e662d8e7c8ad6f02d3
swath-03 <64493233+swath-03@users.noreply.github.com>
2021-02-19 00:53:09 +0000

    added more methods to demo1.java

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -4,16 +4,23 @@ public class Test1 {
 
 
     @Test
-    public void demo1test()
-    {
+    public void demo1test() {
 
-    demo1 obj1 = new demo1();
+        demo1 obj1 = new demo1();
 
-    obj1.add(20,30,0);
+        obj1.add(20, 30, 0);
 
-    obj1.sub(300,100,0);
+    }
 
-    obj1.mul(2,5,0);
+    @Test
+
+     public void demo1test2(){
+
+        demo1 obj2 = new demo1();
+
+    obj2.sub(300,100,0);
+
+    obj2.mul(2,5,0);
 
     }
 }
