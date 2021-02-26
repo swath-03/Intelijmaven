fcc2dcf4f5f095e290e965eb9bd6fdeb87f232e0
swath-03 <64493233+swath-03@users.noreply.github.com>
2021-02-26 23:44:16 +0000

    Added newprogram Number

---
+++ src/main/java/Evenoddnumber.java
@@ -0,0 +1,24 @@
+public class Evenoddnumber {
+
+
+    public String Verifynumber(String counter1,int startvalue,int tillnum) {
+
+
+        System.out.println("the value in the counter is :"+counter1);
+
+        for (int i = startvalue;i <= tillnum;i++)
+        {
+            if(counter1.equals("EVEN")&(startvalue % 2==0))
+            {
+                System.out.println(startvalue);
+            }
+            else if(counter1.equals("ODD")&(startvalue % 2 != 0))
+            {
+                System.out.println(startvalue);
+            }
+            startvalue = startvalue+1;
+        }
+
+     return counter1;
+    }
+}
---
+++ src/test/java/NUmber.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+public class NUmber {
+
+
+   @Test
+    public void Number(){
+
+
+        Evenoddnumber obj1 = new Evenoddnumber();
+
+        obj1.Verifynumber("EVEN",2,28);
+        obj1.Verifynumber("ODD",7,35);
+
+
+
+    }
+}
+
